a8896fb5b02b5af8f0197cb2319c67f9d84a3f85
Richard Mensah <richarda.mensah@gmail.com>
2021-01-09 13:53:56 +0000

    Fixed missing Org Logo in Multi-Org Report Runs

--- app_code/inv/htm_rpts/invoice_rpt.php
+++ app_code/inv/htm_rpts/invoice_rpt.php
@@ -161,7 +161,7 @@ text-align: left !important;
                         <div class=\"row\">
                             <div class=\"col\">
                                 <a target=\"_blank\" href=\"$app_url\">
-                                    <img src=\"".$app_url."/dwnlds/amcharts_2100/images/" . $orgID . ".png\" data-holder-rendered=\"true\" style=\"height:70px !important;width:auto;\"/>
+                                    <img src=\"".$app_url."dwnlds/amcharts_2100/images/" . $orgLogo . "\" data-holder-rendered=\"true\" style=\"height:70px !important;width:auto;\"/>
                                 </a>
                             </div>
                             <div class=\"col company-details\">
